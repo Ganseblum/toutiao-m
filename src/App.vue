3eda58349d47e26d98e555fa615cfb4c33fee3db
Ganseblum <977252487@qq.com>
2022-05-01 20:52:51 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <!-- 路由的出口 -->
     <router-view />
+    <van-cell-group inset>
+    <i class="toutiao toutiao-shipin"></i>
     <h1>黑马头条</h1>
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
